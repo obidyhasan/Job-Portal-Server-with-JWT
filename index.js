a86a421511b9b2a26382f2d4ddd38e3e98956a67
obidyhasan <obidyhasan@gmail.com>
2024-12-18 21:03:47 +0600

    include production info

--- index.js
+++ index.js
@@ -63,7 +63,8 @@ async function run() {
       res
         .cookie("token", token, {
           httpOnly: true,
-          secure: false,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
         })
         .send({ success: true });
     });
@@ -72,7 +73,8 @@ async function run() {
       res
         .clearCookie("token", {
           httpOnly: true,
-          secure: false,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
         })
         .send({ LogoutSuccess: true });
     });
